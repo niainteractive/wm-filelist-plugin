495cb2e280616e58f9881a5d7c41c4b264e4395b
afrimax <afrimax@niainteractive.com>
2024-08-11 20:12:46 -0400

    Create FileList.php
    WinterCMS Plugin.
    
    Displays list of files in a given directory.

---
+++ components/FileList.php
@@ -0,0 +1,37 @@
+// plugins/niainteractive/wn-filelist-plugin/components/FileList.php
+
+namespace NiaInteractive\FileList\Components;
+
+use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\Storage;
+
+class FileList extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'FileList',
+            'description' => 'Displays a list of files in a given directory'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'directory' => [
+                'title'       => 'Directory',
+                'description' => 'Path to the directory to list files from',
+                'default'     => 'media',
+                'type'        => 'string'
+            ]
+        ];
+    }
+
+    public function onRun()
+    {
+        $directory = $this->property('directory');
+        $files = Storage::disk('local')->files($directory);
+
+        $this->page['files'] = $files;
+    }
+}
