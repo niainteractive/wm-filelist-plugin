1f76c0a8b5e46428a6b266a14fd7beeeee339563
afrimax <afrimax@niainteractive.com>
2024-08-11 20:19:52 -0400

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'filelist',
+        'description' => 'Lists all files in a given directory/folder and auto-creates a link to that file. Ideal for displaying contents of large folders on Front end.'
+    ]
+];
